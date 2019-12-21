65992e7b352599ce248e8e0ca0ebe4c05d6fcb8a
Scott Conner <39441634+DaCoolOne@users.noreply.github.com>
2019-12-21 00:48:03 -0600

    S P A C E S
    Also made some minor changes

--- agent.py
+++ agent.py
@@ -1,4 +1,8 @@
 
+"""
+    Main bot code. If you are looking for the actual triple wave dash function,
+    please head to actions.py
+"""
 
 
 from rlbot.agents.base_agent import BaseAgent
@@ -9,49 +13,49 @@ from actions import *
 
 # Rendering is commented out.
 class Bowtie(BaseAgent):
-	
-	def initialize_agent(self):
-		self.packet = Packet()
-		self.controller_state = None
-		self.action = None
-		self.maneuver = Action()
-		self.maneuver_complete = True
-		self.has_removed_ball = False
-	
-	def set_maneuver(self, a):
-		self.maneuver_complete = False
-		self.maneuver = a
-	
-	def get_output(self, gtp):
-		
-		# self.renderer.begin_rendering()
-		
-		if not self.has_removed_ball:
-			ball_state = BallState(Physics(location=Vector3(0, 0, 3000)))
-			game_state = GameState(ball=ball_state)
-			self.set_game_state(game_state)
-			self.has_removed_ball = True
-		
-		self.controller_state = Controller()
-		self.packet.update(gtp)
-		
-		if self.action is None:
-			self.action = Test_Maneuver()
-		
-		my_car = self.packet.game_cars[self.index]
-		
-		if self.maneuver_complete:
-			self.action = self.action.update(self, self.packet)
-			# self.renderer.draw_string_3d(my_car.location.UI_Vec3(), 2, 2, type(self.action).__name__, self.renderer.yellow())
-		else:
-			
-			self.action.idle(self, self.packet)
-			
-			self.maneuver_complete = self.maneuver.update(self, self.packet)
-			# self.renderer.draw_string_3d(my_car.location.UI_Vec3(), 2, 2, type(self.maneuver).__name__, self.renderer.red())
-		
-		# self.renderer.end_rendering()
-		
-		return self.controller_state.get()
-		
-	
+    
+    def initialize_agent(self):
+        self.packet = Packet()
+        self.controller_state = None
+        self.action = None
+        self.maneuver = Action()
+        self.maneuver_complete = True
+        self.has_removed_ball = False
+    
+    def set_maneuver(self, a):
+        self.maneuver_complete = False
+        self.maneuver = a
+    
+    def get_output(self, gtp):
+        
+        # self.renderer.begin_rendering()
+        
+        if not self.has_removed_ball:
+            ball_state = BallState(Physics(location=Vector3(0, 0, 3000)))
+            game_state = GameState(ball=ball_state)
+            self.set_game_state(game_state)
+            self.has_removed_ball = True
+        
+        self.controller_state = Controller()
+        self.packet.update(gtp)
+        
+        if self.action is None:
+            self.action = Test_Maneuver()
+        
+        my_car = self.packet.game_cars[self.index]
+        
+        if self.maneuver_complete:
+            self.action = self.action.update(self, self.packet)
+            # self.renderer.draw_string_3d(my_car.location.UI_Vec3(), 2, 2, type(self.action).__name__, self.renderer.yellow())
+        else:
+            
+            self.action.idle(self, self.packet)
+            
+            self.maneuver_complete = self.maneuver.update(self, self.packet)
+            # self.renderer.draw_string_3d(my_car.location.UI_Vec3(), 2, 2, type(self.maneuver).__name__, self.renderer.red())
+        
+        # self.renderer.end_rendering()
+        
+        return self.controller_state.get()
+        
+    
--- controller.py
+++ controller.py
@@ -1,75 +1,87 @@
 
+
+"""
+    Various controllers I use to make life easier.
+"""
+
 from constants import MAX_CAR_VEL
 from structs import *
 from extra_math import *
 
-def Align_Car_To(self, packet, vector, up = Vec3(0, 0, 0)):
-	
-	my_car = packet.game_cars[self.index]
-	
-	car_rot = my_car.rotation
-	
-	car_rot_vel = my_car.angular_velocity
-	
-	local_euler = car_rot_vel.align_from(car_rot)
-	
-	align_local = vector.align_from(car_rot)
-	
-	local_up = up.align_from(car_rot)
-	
-	# Improving this
-	rot_ang_const = 0.25
-	stick_correct = 6.0
-	
-	a1 = math.atan2(align_local.y, align_local.x)
-	a2 = math.atan2(align_local.z, align_local.x)
-	
-	if local_up.y == 0 and local_up.z == 0:
-		a3 = 0.0
-	else:
-		a3 = math.atan2(local_up.y, local_up.z)
-	
-	yaw = correct(0.0, -a1 + local_euler.z * rot_ang_const, stick_correct)
-	pitch = correct(0.0, -a2 - local_euler.y * rot_ang_const, stick_correct)
-	roll = correct(0.0, -a3 - local_euler.x * rot_ang_const, stick_correct)
-	
-	self.controller_state.yaw = clamp_1(yaw)
-	self.controller_state.pitch = clamp_1(pitch)
-	self.controller_state.roll = clamp_1(roll)
-	
-	self.controller_state.steer = clamp_1(yaw)
+"""
+    Aligns the car to a forward, up vector pair. If the up vector is not defined, then
+    the function will simply keep the roof facing teh same way it has been facing.
+"""
+def Align_Car_To(self, packet, vector, up = Vec3()):
+    
+    my_car = packet.game_cars[self.index]
+    
+    car_rot = my_car.rotation
+    
+    car_rot_vel = my_car.angular_velocity
+    
+    local_euler = car_rot_vel.align_from(car_rot)
+    
+    align_local = vector.align_from(car_rot)
+    
+    local_up = up.align_from(car_rot)
+    
+    # Improving this
+    rot_ang_const = 0.25
+    stick_correct = 6.0
+    
+    a1 = math.atan2(align_local.y, align_local.x)
+    a2 = math.atan2(align_local.z, align_local.x)
+    
+    if local_up.y == 0 and local_up.z == 0:
+        a3 = 0.0
+    else:
+        a3 = math.atan2(local_up.y, local_up.z)
+    
+    yaw = correct(0.0, -a1 + local_euler.z * rot_ang_const, stick_correct)
+    pitch = correct(0.0, -a2 - local_euler.y * rot_ang_const, stick_correct)
+    roll = correct(0.0, -a3 - local_euler.x * rot_ang_const, stick_correct)
+    
+    self.controller_state.yaw = clamp_1(yaw)
+    self.controller_state.pitch = clamp_1(pitch)
+    self.controller_state.roll = clamp_1(roll)
+    
+    self.controller_state.steer = clamp_1(yaw)
 
-# I always write one of these
+
+"""
+    It drives
+"""
 def drive(agent, packet, target, time = 0):
-	my_car = packet.game_cars[agent.index]
-	car_to_pos = target - (my_car.location - my_car.hitbox_offset.align_to(my_car.rotation))
-	target_vel = car_to_pos.length() / not_zero(time) # * sign(my_car.velocity.dot(car_to_pos))
-	actual_vel = my_car.velocity.dot(car_to_pos.normal())
-	
-	c_p_l = car_to_pos.align_from(my_car.rotation)
-	
-	up = Vec3(z=1).align_to(my_car.rotation)
-	
-	s_val = math.atan2(c_p_l.y, c_p_l.x)
-	
-	agent.controller_state.steer = clamp_1(s_val * 3)
-	
-	if abs(s_val) > math.pi * 0.4 and my_car.velocity.length() > 400 and up.z > 0.8:
-		agent.controller_state.handbrake = True
-		agent.controller_state.throttle = 1
-	elif target_vel > actual_vel:
-		agent.controller_state.throttle = 1
-		agent.controller_state.boost = my_car.velocity.length() < MAX_CAR_VEL - 5 and my_car.has_wheel_contact
-	elif target_vel > actual_vel + 100:
-		agent.controller_state.throttle = 1
-		agent.controller_state.boost = False
-	elif target_vel > actual_vel + 400:
-		agent.controller_state.throttle = 0.3
-		agent.controller_state.boost = False
-	else:
-		agent.controller_state.throttle = 0
-		agent.controller_state.boost = False
-	
+    my_car = packet.game_cars[agent.index]
+    car_to_pos = target - (my_car.location - my_car.hitbox_offset.align_to(my_car.rotation))
+    target_vel = car_to_pos.length() / not_zero(time) # * sign(my_car.velocity.dot(car_to_pos))
+    actual_vel = my_car.velocity.dot(car_to_pos.normal())
+    
+    c_p_l = car_to_pos.align_from(my_car.rotation)
+    
+    up = Vec3(z=1).align_to(my_car.rotation)
+    
+    s_val = math.atan2(c_p_l.y, c_p_l.x)
+    
+    agent.controller_state.steer = clamp_1(s_val * 3)
+    
+    if abs(s_val) > math.pi * 0.4 and my_car.velocity.length() > 400 and up.z > 0.8:
+        agent.controller_state.handbrake = True
+        agent.controller_state.throttle = 1
+    elif target_vel > actual_vel:
+        agent.controller_state.throttle = 1
+        agent.controller_state.boost = my_car.velocity.length() < MAX_CAR_VEL - 5 and my_car.has_wheel_contact
+    elif target_vel > actual_vel + 100:
+        agent.controller_state.throttle = 1
+        agent.controller_state.boost = False
+    elif target_vel > actual_vel + 400:
+        agent.controller_state.throttle = 0.3
+        agent.controller_state.boost = False
+    else:
+        agent.controller_state.throttle = 0
+        agent.controller_state.boost = False
+    
 
 
 
